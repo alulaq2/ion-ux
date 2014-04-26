030eecc7a5bc6b31b75e3526abe5a2fb3f40942a
gkourupes <gkourupes@yahoo.com>
2014-04-25 20:32:38 -0400

    Got legacy to work mostly... needs a few fixes.

--- main.py
+++ main.py
@@ -101,8 +101,8 @@ def login_required(f):
 
 @app.route('/')
 def index():
-    return render_template('ion_ux4.html')
-    # return render_app_template(request.path)
+    # return render_template('ion_ux3.html')
+    return render_app_template(request.path)
 
 @app.route('/failed')
 def failed_login():
--- static/js/ion_ux2.js
+++ static/js/ion_ux2.js
@@ -1,30 +1,3 @@
-// Timestamp conversion methods to call when parsing response.
-// Maybe put these into IONUX2.Helpers namespace?
-var epoch_to_iso = function(epoch_time){
-    try {
-      return new Date(parseInt(epoch_time)).toISOString().replace(/T/, ' ').replace(/.\d{3}Z$/, 'Z');
-    } catch (err) {
-      return epoch_time;
-    };
-};
-
-var make_iso_timestamps = function(resp) {
-  if(resp != null && 'type_' in resp) {
-    if('ts_created' in resp) {
-      resp['ts_created'] = epoch_to_iso(resp['ts_created']);
-    }
-    if('ts_updated' in resp) {
-      resp['ts_updated'] = epoch_to_iso(resp['ts_updated']);
-    }
-  }
-
-  _.each(resp, function(val, key) {
-      if (typeof val == 'object') {
-        make_iso_timestamps(val);
-      };
-  });
-  return;
-};
 
 var get_template = function(url) {
     var data = "<h1> failed to load url : " + url + "</h1>";
@@ -54,34 +27,37 @@ IONUX2 = {
 		IONUX2.Models.SessionInstance = new IONUX2.Models.Session();
 		IONUX2.Models.HeaderInstance = new IONUX2.Models.Header();
 
+		
 		IONUX2.Views.HeaderInstance = new IONUX2.Views.Header({model: IONUX2.Models.HeaderInstance});
 
 		IONUX2.Models.HeaderInstance.fetch({
 			async: false,
 			dataType: 'html'
 		});
-
+		
 		IONUX2.Models.SearchTabContentInstance = new IONUX2.Models.SearchTabContent();
 		IONUX2.Views.SearchTabContentInstance = new IONUX2.Views.SearchTabContent({model: IONUX2.Models.SearchTabContentInstance});
 		IONUX2.Models.SearchTabContentInstance.fetch({
 			async: false,
 			dataType: 'html'
 		});
-
+		
 		$("#leftSubheader").html(IONUX2.getTemplate('templates/block_nav_tabs2.html')).show();
 		$("#lowerMain").html(IONUX2.getTemplate('templates/block_accordion_white2.html')).show();
-
+		$("#legacyTabContent").html(IONUX2.getTemplate('templates/block_legacy_nav2.html')).show();
+		
 	    // Bootstrap navigation menu
 	    $.ajax({
 			async: false,
 			url: '/ui/navigation/',
 			success: function(resp) {
 		        // MAPS Sidebar (initially shown)
-		        IONUX2.Dashboard.Observatories = new IONUX2.Collections.Observatories(_.sortBy(resp.data.observatories,function(o){return o.spatial_area_name + (o.local_name ? o.local_name : '') + o.name}))
-      		},
+		        IONUX2.Dashboard.Observatories = new IONUX2.Collections.Observatories(_.sortBy(resp.data.observatories,function(o){return o.spatial_area_name + (o.local_name ? o.local_name : '') + o.name}));
+      		}
       	});
+      	
 	    IONUX2.Views.spatial = new IONUX2.Views.Spatial();
-      	this.dashboard_map();
+      	this.dashboard_map();      	
 	},
 
 	getTemplate: function(url) {
@@ -112,7 +88,7 @@ IONUX2 = {
 	      });
 	    }else{
 	      IONUX2.Dashboard.MapView.active_marker = null; // Track clicked icon
-	      IONUX2.Dashboard.MapView.map_bounds_elmt = $('#map_bounds');
+	      IONUX2.Dashboard.MapView.map_bounds_elmt = $('#map_bounds2');
 	      IONUX2.Dashboard.MapView.draw_map();
 	      IONUX2.Dashboard.MapView.draw_markers();
 	    }
