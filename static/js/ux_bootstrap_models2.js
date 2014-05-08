c5d03ede46219269f589b04fa1b378e0c56b40ef
Carlos Hinojosa <carloscience@gmail.com>
2014-05-07 20:43:28 -0400

    convert saved searches to collection

--- static/js/ux_bootstrap_models2.js
+++ static/js/ux_bootstrap_models2.js
@@ -212,7 +212,7 @@ IONUX2.Models.instruments = new IONUX2.Models.Instruments();
 
 IONUX2.Models.SaveSpatialSearch = Backbone.Model.extend({
   defaults: {
-      accordion_visible: false,
+      spatial_accordion_visible: false,
       spatial_dropdown: "1",
       from_latitude: "",
       from_ns: "",
@@ -265,7 +265,7 @@ IONUX2.Models.saveSpatialSearch = new IONUX2.Models.SaveSpatialSearch();
 
 IONUX2.Models.SaveTemporalSearch = Backbone.Model.extend({
   defaults: {
-    accordion_visible: false,
+    temporal_accordion_visible: false,
     temporal_dropdown: '',
     from_year: '',
     from_month: '',
