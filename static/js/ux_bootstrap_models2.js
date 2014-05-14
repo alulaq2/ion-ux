2a13fa036e16f252ebcb66d2b7420f945b60dc8a
Carlos Hinojosa <carloscience@gmail.com>
2014-05-14 19:20:06 -0400

    saving sort order to model

--- static/js/ux_bootstrap_models2.js
+++ static/js/ux_bootstrap_models2.js
@@ -311,6 +311,9 @@ IONUX2.Models.SaveConfiguration = Backbone.Model.extend({
 });
 IONUX2.Models.saveConfiguration = new IONUX2.Models.SaveConfiguration();
 
+IONUX2.Models.SortOrder = Backbone.Model.extend({});
+IONUX2.Models.sortModelInstance = new IONUX2.Models.SortOrder;
+
 IONUX2.Collections.SaveNames = Backbone.Collection.extend({});
 
 IONUX2.Collections.saveNames = new IONUX2.Collections.SaveNames();
