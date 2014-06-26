c699deae99636b92d68d13eb5a1b669e94bb7933
gkourupes <gkourupes@yahoo.com>
2014-06-25 20:22:29 -0400

    Added observatories and unassigned items.

--- main.py
+++ main.py
@@ -176,8 +176,9 @@ def user_configuration(user_id):
 @app.route('/test_query/', methods=['POST'])
 def test_query_post_passthrough():
     # query_string = request.form['payload']
-    query_string = request.json['payload']
-    results = ServiceApi.test_query_post_passthrough(query_string)
+    # query = json.loads(query_string)
+    query = request.json['payload']
+    results = ServiceApi.test_query_post_passthrough(query)
     return render_json_response(results)
 
 
